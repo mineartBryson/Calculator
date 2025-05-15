4662fc9bf4444ec2f40b701dcaec12ba0ffd92bc
mineartBryson <mineartbryson@gmail.com>
2025-05-14 21:14:57 -0700

    Refined calculator styling and layout improvements

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,29 +1,39 @@
 import Calculate from '../../services/calculate';
-import './Button.css'
+import './Button.css';
 
 const Button = ({ onButtonClick }) => {
     const numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
     const operators = ['+', '-', '*', '/', '=', 'Clear'];
 
     return (
-    <div className="button_container">
-        <div className="number_buttons">
-        {numbers.map(value => (
-            <button key={value} onClick={() => onButtonClick(value)} id={value}>
-            {value}
-            </button>
-        ))}
+        <div className="calculator__buttons">
+            <div className="calculator__number-buttons">
+                {numbers.map(value => (
+                    <button 
+                        key={value} 
+                        className="calculator__button calculator__button--number"
+                        onClick={() => onButtonClick(value)} 
+                        id={value}
+                    >
+                        {value}
+                    </button>
+                ))}
+            </div>
+            <div className="calculator__operator-buttons">
+                {operators.map(value => (
+                    <button 
+                        key={value} 
+                        className="calculator__button calculator__button--operator"
+                        onClick={() => onButtonClick(value)} 
+                        id={value}
+                    >
+                        {value}
+                    </button>
+                ))}
+            </div>
         </div>
-        <div className="operator_buttons">
-        {operators.map(value => (
-            <button key={value} onClick={() => onButtonClick(value)} id={value}>
-            {value}
-            </button>
-        ))}
-        </div>
-    </div>
+
     );
 }
-
 
 export default Button;
