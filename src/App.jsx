451a7576d1caa803988eb6107c8af117f9f3eaca
mineartBryson <mineartbryson@gmail.com>
2025-05-19 15:47:59 -0700

    Added comments and explanations to the files

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
+// App.jsx
+// Primary house for the calculator application.
+
 import { useState } from 'react';
 import './App.css';
 import Button from './components/Button';
 import Calculate from './services/calculate';
 
 function App() {
+  // Tracking state for the expression - where the expression is the math that the user has typed in
   const [expression, setExpression] = useState([]);
+  // Hold the number that the user has typed in
   const [currentNumber, setCurrentNumber] = useState("");
 
   const handleButtonClick = (value) => {
--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,13 +1,17 @@
+// Button.jsx
+// Component used for all of the calculator buttons: numbers, operators, and clear
 import Calculate from '../../services/calculate';
 import './Button.css';
 
 const Button = ({ onButtonClick }) => {
+    // St up a list of all the currently supported operators and a separate list of the numbers for the clickable butons
     const numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
     const operators = ['+', '-', '*', '/', '=', 'Clear'];
 
     return (
         <div className="calculator__buttons">
             <div className="calculator__number-buttons">
+                {/* Sets up the number buttons */}
                 {numbers.map(value => (
                     <button 
                         key={value} 
@@ -19,6 +23,7 @@ const Button = ({ onButtonClick }) => {
                     </button>
                 ))}
             </div>
+            {/* Sets up the operator buttons and other functions */}
             <div className="calculator__operator-buttons">
                 {operators.map(value => (
                     <button 
